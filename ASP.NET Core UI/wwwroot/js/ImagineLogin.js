87aadded5117fa1d039f1d51b9df5d7f930680ce
Mandel Andrei Nitu <andrei.nitu@essensys.ro>
2019-08-21 15:17:21 +0300

    Am pus Layout-ul general in toate paginile inafara de login si register si am facut in asa fel incat imaginea din ferestrele de login si register sa nu fie mai mare decat ecranul si de asemenea sa nu  se intersecteze

---
+++ ASP.NET Core UI/wwwroot/js/ImagineLogin.js
@@ -0,0 +1,8 @@
+﻿window.addEventListener("load", () => {
+
+    inaltimeTotala = window.innerHeight;
+    inaltimeHeader = $("header").height();
+    inaltimeFooter = $("footer").height();
+    inaltimaMaximaPoza = inaltimeTotala - inaltimeHeader - inaltimeFooter - 100;
+    $("#imagineLogin").height(inaltimaMaximaPoza);
+})
--- ASP.NET Core UI/wwwroot/js/Layout.js
+++ ASP.NET Core UI/wwwroot/js/Layout.js
@@ -14,8 +14,13 @@
 
                     var redirect = '/Profile/Profile' + "?userId=" + response[i].id;
                     var photoURL = '/Photos/Download' + "/" + response[i].profilePhotoId;
-                    $('#users').append(`<a href=${redirect}><div class="item"><img src=${photoURL} height="100em" /> <p>${response[i].name}</p>  </div></a>`)
-
+                    if (response[i].profilePhotoId !== null) {
+                        
+                        $('#users').append(`<a href=${redirect}><div class="item"><img src=${photoURL} height="100em" /> <p>${response[i].name}</p>  </div></a>`)
+                    }
+                    else {
+                        $('#users').append(`<a href=${redirect}><div class="item"><img src="/images/DefaultProfile.png" height="100em" /> <p>${response[i].name}</p>  </div></a>`)
+                    }
                 }
 
             },
